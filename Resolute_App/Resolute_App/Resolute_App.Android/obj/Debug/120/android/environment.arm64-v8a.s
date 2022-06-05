06f836dc78d06ec0844b109742c3c1b95e30f4e6
zrta2480 <96914273+zrta2480@users.noreply.github.com>
2022-06-05 21:03:31 +0800

    Added login with google button

--- Resolute_App/Resolute_App/Resolute_App.Android/obj/Debug/120/android/environment.arm64-v8a.s
+++ Resolute_App/Resolute_App/Resolute_App.Android/obj/Debug/120/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"acafa0a2-ce99-403f-b121-06ad06150bbc"
+	.asciz	"7ae1f94e-f95a-4b98-bf51-69f8f91a91a2"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
