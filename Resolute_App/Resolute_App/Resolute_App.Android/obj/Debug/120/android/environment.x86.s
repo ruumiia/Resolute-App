b587de0e5d5e08008cc7dd57bc139db50caebb62
zrta2480 <96914273+zrta2480@users.noreply.github.com>
2022-06-05 20:40:00 +0800

    Changed Tabs
    Added icons to tab options

--- Resolute_App/Resolute_App/Resolute_App.Android/obj/Debug/120/android/environment.x86.s
+++ Resolute_App/Resolute_App/Resolute_App.Android/obj/Debug/120/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"4b4bd9d6-e9f5-4bce-aea5-cf469e61fc89"
+	.asciz	"acafa0a2-ce99-403f-b121-06ad06150bbc"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
