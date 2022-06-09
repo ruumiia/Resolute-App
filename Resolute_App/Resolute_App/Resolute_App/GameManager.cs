7dccbed10547e6e261465e7c375d8e65867eaafe
ruumiia <lkaneki.kenl@gmail.com>
2022-06-09 18:59:24 +0800

    compile

---
+++ Resolute_App/Resolute_App/Resolute_App/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Resolute_App
+{
+    class GameManager
+    {
+        private List<Question> questions;
+        private int score;
+        private Question currentQuestion;
+        private int questionCount;
+        public int CorrectCount { get; set; }
+        public int WrongCount { get; set; }
+        public int LifeAmount { get; set; } = 3;
+
+        public GameManager()
+        {
+            currentQuestion = new Question(5);
+            questionCount++;
+        }
+
+        public Question getCurrentQuestion()
+        {
+            return this.currentQuestion;
+        }
+
+        public void generateQuestion()
+        {
+            currentQuestion = new Question(questionCount + 5 * 2);
+            questionCount++;
+        }
+
+        public void checkAnswer(int answerArg)
+        {
+            if (answerArg == this.currentQuestion.getAnswer())
+            {
+                CorrectCount++;
+            }
+            else
+            {
+                WrongCount++;
+                LifeAmount--;
+            }
+        }
+
+
+    }
+}
