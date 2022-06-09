9f238b4ba92f47dcdd0a3176f4771f0ed3780f26
ruumiia <lkaneki.kenl@gmail.com>
2022-06-09 19:12:21 +0800

    commit

--- Resolute_App/Resolute_App/Resolute_App/obj/Release/netstandard2.0/Login.xaml.g.cs
+++ Resolute_App/Resolute_App/Resolute_App/obj/Release/netstandard2.0/Login.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Resolute_App {
     public partial class Login : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button LoginBtn;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Login));
+            LoginBtn = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "LoginBtn");
         }
     }
 }
--- Resolute_App/Resolute_App/Resolute_App/obj/Release/netstandard2.0/MainPage.xaml.g.cs
+++ Resolute_App/Resolute_App/Resolute_App/obj/Release/netstandard2.0/MainPage.xaml.g.cs
@@ -17,20 +17,8 @@ namespace Resolute_App {
     public partial class MainPage : global::Xamarin.Forms.TabbedPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Button LoginPage;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Button MessagingUI;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Button ChatboxUI;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            LoginPage = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "LoginPage");
-            MessagingUI = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "MessagingUI");
-            ChatboxUI = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "ChatboxUI");
         }
     }
 }
